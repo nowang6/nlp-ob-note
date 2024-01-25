6bb9bebfbaca11173b5a6ba8b4945f8b899371ff
nowang6 <150778393+nowang6@users.noreply.github.com>
2024-01-25 16:49:15 +0800

    Create langchain.py

---
+++ 6LLM/langchain.py
@@ -0,0 +1,11 @@
+# 模块
+Model I/O
+Retrieval
+Agents
+Chains
+Memory
+Callbacks
+
+基础层：models、LLMs、index  -> Model I/O +  Retrieval
+能力层：Chains、Memory、Tools -> Chains、Memory、Callbacks
+应用层：Agents -> Agent
